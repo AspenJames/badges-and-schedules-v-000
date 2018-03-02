9d6a8333a10a3d1fb257f052381eb5eca4bb29ec
Aspen James <aspenjames@tqca.org>
2018-03-02 18:42:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index{ |name, index|
     room = index + 1
-    room_assignments << "Hello #{name}! You'll be assigned to room #{room}!"
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
   }
   room_assignments
 end
