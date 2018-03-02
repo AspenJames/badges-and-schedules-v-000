8d248a87e3d401c7a8b51f16074b6739cd973fd7
Aspen James <aspenjames@tqca.org>
2018-03-02 18:38:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def conference_badges(names)
+def batch_badge_creator(names)
   badges = []
   names.each { |name|
     badges << badge_maker(name)
